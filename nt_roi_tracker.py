1394396095f60e03e141c9f5d52219afb5438885
Chike <40407778+see-k@users.noreply.github.com>
2022-11-06 12:20:55 -0600

    Add files via upload

---
+++ nt_roi_tracker.py
@@ -0,0 +1,18 @@
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+def get_return(returns, investment, days):
+    for i in range(days):
+        if (i % 7 == 0):
+            print(f'\nDay {i} = {returns}')
+        returns = (returns * (investment/100)) + returns
+    return returns
+    
+
+capital = float(input('Capital investment: '))
+interest_rate = float(input('Compounding interest: '))
+total_time = int(input('number of days: '))
+
+print(f'\nThe total return for a compounding interest rate of {interest_rate}% starting with an initial capital investment of ${capital} is ${get_return(capital, interest_rate, total_time)}')
